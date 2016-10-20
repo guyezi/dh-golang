ab3949666eaf05df0b7646e3c51651b66697e2aa
Martín Ferrari <tincho@debian.org>
2016-10-21 00:46:16 +0200

    Stop forcing the compression to xz.

--- lib/Debian/Debhelper/Sequence/golang.pm
+++ lib/Debian/Debhelper/Sequence/golang.pm
@@ -5,9 +5,4 @@ use Debian::Debhelper::Dh_Lib;
 
 insert_before('dh_gencontrol', 'dh_golang');
 
-# XXX: -u is deprecated, but we cannot use “-- -Zxz” because additional command
-# options will be appended (“-O--buildsystem=golang”), resulting in
-# “dh_builddeb -- -Zxz -O--buildsystem=golang”, which fails.
-add_command_options('dh_builddeb', '-u-Zxz');
-
 1
