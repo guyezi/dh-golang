75ceaac6c1ce453288eee19cea170b84f34c613f
Michael Stapelberg <stapelberg@debian.org>
2017-10-10 22:33:25 +0200

    Document Debian::Debhelper::Buildsystem::golang (Closes: #839108)

--- lib/Debian/Debhelper/Buildsystem/golang.pm
+++ lib/Debian/Debhelper/Buildsystem/golang.pm
@@ -1,5 +1,206 @@
 package Debian::Debhelper::Buildsystem::golang;
 
+=head1 NAME
+
+dh-golang -- debhelper build system class for Go packages
+
+=head1 DESCRIPTION
+
+The dh-golang package provides a build system for debhelper which can be used in
+the following way:
+
+ %:
+ 	dh $@ --buildsystem=golang --with=golang
+
+=head1 IMPLEMENTATION
+
+Here is a brief description of how the golang build system implements each
+debhelper build system stage:
+
+=over
+
+=item B<configure>
+
+Creates a Go workspace (see https://golang.org/doc/code.html#Workspaces) in the
+build directory. Copies the source code into that workspace and symlinks all
+available libraries from /usr/share/gocode/src into the workspace because the
+go(1) tool requires write access to the workspace. See also
+C<DH_GOLANG_INSTALL_EXTRA> and C<DH_GOLANG_INSTALL_ALL>.
+
+=item B<build>
+
+Determines build targets (see also C<DH_GOLANG_BUILDPKG> and
+C<DH_GOLANG_EXCLUDES>), possibly calls C<go generate> (see also
+C<DH_GOLANG_GO_GENERATE>), then calls C<go install>.
+
+=item B<test>
+
+Calls C<go test -v> on all build targets.
+
+=item B<install>
+
+Installs binaries and sources from the build directory into the Debian package
+destdir. See also C<--no-source> and C<--no-binaries>.
+
+=item B<clean>
+
+Removes the build directory.
+
+=back
+
+=head1 OPTIONS
+
+=over
+
+=item B<--no-source>
+
+By default, all files within the src/ subdirectory of the build directory will
+be copied to /usr/share/gocode/src/ of the Debian package destdir. Specifying
+the C<--no-source> option disables this behavior, which is useful if you are
+packaging a program (as opposed to a library).
+
+Example (in C<debian/rules>):
+
+ %:
+ 	dh $@ --buildsystem=golang --with=golang --no-source
+
+=item B<--no-binaries>
+
+By default, all files within the bin/ subdirectory of the build directory will
+be copied to /usr/bin/ of the Debian package destdir. Specifying the
+C<--no-binaries> option disables this behavior.
+
+Example (in C<debian/rules>):
+
+ %:
+ 	dh $@ --buildsystem=golang --with=golang --no-binaries
+
+Note: instead of using this option (which was added for symmetry with
+C<--no-source>), consider not building unwanted binaries in the first place to
+save CPU time on our build daemons; see C<DH_GOLANG_EXCLUDES>.
+
+=back
+
+=head1 ENVIRONMENT VARIABLES
+
+=over
+
+=item C<DH_GOPKG>
+
+C<DH_GOPKG> (string) contains the Go package name which this Debian package is
+building.
+
+C<DH_GOPKG> is automatically set to the value of the C<XS-Go-Import-Path>
+C<debian/control> field.
+
+Example (in C<debian/control>):
+
+ XS-Go-Import-Path: github.com/debian/ratt
+
+Historical note: before the C<XS-Go-Import-Path> field was introduced, we used
+to set C<DH_GOPKG> in C<debian/rules>. When you encounter such a package, please
+convert it by moving the value from C<debian/rules> to C<debian/control>. It is
+preferable to use the C<debian/control> field because it is machine-readable and
+picked up/used by various Debian infrastructure tools, whereas C<debian/rules> is
+very hard to parse.
+
+=item DH_GOLANG_INSTALL_EXTRA
+
+C<DH_GOLANG_INSTALL_EXTRA> (list of strings, whitespace-separated, default
+empty) enumerates files and directories which are additionally installed into
+the build directory. By default, only files with the following extension are
+installed: .go, .c, .cc, .h, .hh, .proto, .s.
+
+Example (in C<debian/rules>):
+
+ export DH_GOLANG_INSTALL_EXTRA := html/charset/testdata html/testdata \
+    bpf/testdata
+
+=item DH_GOLANG_INSTALL_ALL
+
+C<DH_GOLANG_INSTALL_ALL> (bool, default false) controls whether all files are
+installed into the build directory. By default, only files with the following
+extension are installed: .go, .c, .cc, .h, .hh, .proto, .s.
+
+Example (in C<debian/rules>):
+
+ export DH_GOLANG_INSTALL_ALL := 1
+
+Note: prefer the C<DH_GOLANG_INSTALL_EXTRA> environment variable because it is
+self-documenting and future-proof: when using C<DH_GOLANG_INSTALL_ALL>, readers
+of your package cannot easily tell which extra files in particular need to be
+installed, and newer upstream versions might result in unexpected extra files.
+
+=item DH_GOLANG_BUILDPKG
+
+C<DH_GOLANG_BUILDPKG> (list of strings, whitespace-separated, default
+C<${DH_GOPKG}/...>) defines the build targets for compiling this Go package. In
+other words, this is what will be passed to C<go install>.
+
+The default value matches all Go packages within the source, which is usually
+desired, but you might need to exclude example programs, for which you should
+use the C<DH_GOLANG_EXCLUDES> environment variable.
+
+Example (in C<debian/rules>):
+
+ # Install only programs for end users, the also-included Go packages are not
+ # yet mature enough to be shipped for other packages to consume (despite what
+ # upstream claims).
+ export DH_GOLANG_BUILDPKG := github.com/debian/ratt/cmd/...
+
+=item DH_GOLANG_EXCLUDES
+
+C<DH_GOLANG_EXCLUDES> (list of Perl regular expressions, whitespace-separated,
+default empty) defines regular expression patterns to exclude from the build
+targets expanded from C<DH_GOLANG_BUILDPKG>.
+
+Example (in C<debian/rules>):
+
+ # We want to ship only the library packages themselves, not the accompanying
+ # example binaries.
+ export DH_GOLANG_EXCLUDES := examples/
+
+=item DH_GOLANG_GO_GENERATE
+
+C<DH_GOLANG_GO_GENERATE> (bool, default false) controls whether C<go generate>
+is called on all build targets (see C<DH_GOLANG_BUILDPKG>).
+
+It is convention in the Go community to commit all C<go generate> artifacts to
+version control, so re-generating these artifacts is usually not required.
+
+Depending on what the Go package in question uses C<go generate> for, you may
+want to enable C<DH_GOLANG_GO_GENERATE>:
+
+=over
+
+=item *
+
+If the Go package uses C<go generate> to generate artifacts purely from inputs
+within its own source (e.g. creating a perfect hash table), there usually is no
+need to re-generate that output. It does not necessarily hurt, either, but some
+C<go generate> commands might be poorly tested and break the build.
+
+=item *
+
+If the Go package uses C<go generate> to (e.g.) bundle a JavaScript library into
+a template file which is then compiled into a Go program, it is advisable to
+re-generate that output so that the Debian version of the JavaScript library is
+picked up, as opposed to the pre-generated version.
+
+=back
+
+Example (in C<debian/rules>):
+
+ export DH_GOLANG_GO_GENERATE := 1
+
+Note: this option should default to true, but it was introduced after dh-golang
+was already widely used, and nobody made the transition happen yet (i.e. inspect
+and possibly fix any resulting breakages).
+
+=back
+
+=cut
+
 use strict;
 use base 'Debian::Debhelper::Buildsystem';
 use Debian::Debhelper::Dh_Lib; 
